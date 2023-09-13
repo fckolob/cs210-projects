9e6ef759b9bf8ce91a0dcbd3e5770f7b947e4729
fckolob <fckolob@gmail.com>
2023-09-13 19:03:07 -0300

    prep 1 finished

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Ask the user his name and last name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        //Show the name in the James bond way
+        Console.Write($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
