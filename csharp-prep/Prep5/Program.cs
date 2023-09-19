9ab7ddce9e2346b0fc58d466c0e6086143c65114
fckolob <fckolob@gmail.com>
2023-09-18 21:22:10 -0300

    prep 5 finished

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
-    }
+        static void DisplayWelcome()
+        {
+            Console.WriteLine
+            ("Welcome to the program!");
+        }
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+            return userName;
+        }
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int userNumber = int.Parse(Console.ReadLine());
+            return userNumber;
+            }
+        static int SquareNumber(int theNumber)
+        {
+            int squared = theNumber * theNumber;
+            return squared;
+        }
+        static void DisplayResult(string userName, int squared)
+        {
+            Console.WriteLine($"{userName}, the square of your number is {squared}");
+        }
+        DisplayWelcome();
+        DisplayResult(PromptUserName(), SquareNumber(PromptUserNumber()));
+    }   
 }
