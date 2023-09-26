177d24f63f49b625a37dba58b42f97983bc70a1d
fckolob <131497920+fckolob@users.noreply.github.com>
2023-09-25 21:11:02 -0300

    learning 2 finished

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 1997;
+        job1._endYear = 2005;
+        
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2005;
+        job2._endYear = 2020;
+
+        Resume myResume = new Resume();
+        myResume._name = "Jhon Hammond";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
+
+
     }
+        
+          
+      
 }
