0adee2da4a6f0d339e1a66077a7ba7f754fef209
fckolob <131497920+fckolob@users.noreply.github.com>
2023-10-09 21:32:11 -0300

    prepare week 5 added

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+        Fraction fraction2 = new Fraction();
+        
+        fraction2.SetTop(6);
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+        Fraction fraction3 = new Fraction();
+        fraction3.SetTop(6);
+        fraction3.SetBottom(7);
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+        Fraction fraction4 = new Fraction(1);
+        Console.WriteLine(fraction4.GetFractionString());
+        Console.WriteLine(fraction4.GetDecimalValue());
+        Fraction fraction5 = new Fraction(5);
+        Console.WriteLine(fraction5.GetFractionString());
+        Console.WriteLine(fraction5.GetDecimalValue());
+        Fraction fraction6 = new Fraction(3,4);
+        Console.WriteLine(fraction6.GetFractionString());
+        Console.WriteLine(fraction6.GetDecimalValue());
+        Fraction fraction7 = new Fraction(1,3);
+        Console.WriteLine(fraction7.GetFractionString());
+        Console.WriteLine(fraction7.GetDecimalValue());
+
     }
 }
